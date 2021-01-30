5e2fafe43d2dcf8f41d8831cd2ff085a87f815b9
ayselmehmetzade <ayselmehmetzade@hotmail.com>
2021-01-31 00:10:59 +0300

    Add project files.

---
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -0,0 +1,70 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryProductDal : IProductDal
+
+    {
+
+        List<Product> _products;
+        public InMemoryProductDal()
+        {
+            _products = new List<Product> {
+                new Product{ProductId=1, CtaegoryId=1, ProductName="Bardak", UnitPrice=15, UnitsInStock=15 },
+                new Product{ProductId=2, CtaegoryId=1, ProductName="Kamera", UnitPrice=500, UnitsInStock=3 },
+                new Product{ProductId=3, CtaegoryId=2, ProductName="Telefon", UnitPrice=1500, UnitsInStock=2 },
+                new Product{ProductId=4, CtaegoryId=2, ProductName="Klavye", UnitPrice=150, UnitsInStock=65 },
+                new Product{ProductId=5, CtaegoryId=2, ProductName="Fare", UnitPrice=85, UnitsInStock=1 },
+            };
+        }
+
+        public void Add(Product product)
+        {
+            _products.Add(product);
+        }
+
+        public void Delete(Product product)
+        {
+            //  _products.Remove(product); silinmez çünkğ referans tiptir. 
+
+            //Linq olmasaydı
+            //foreach (var p in _products)
+            //Product productToDelete = null;
+            //{
+            //    if (product.ProductId==p.ProductId)
+            //    {
+            //        productToDelete = p;
+            //    }
+            //}
+
+            Product productToDelete = _products.SingleOrDefault(p=>p.ProductId==product.ProductId);
+            _products.Remove(productToDelete);
+        }
+
+        public List<Product> GetAll()
+        {
+            return _products;
+        }
+
+        public List<Product> GetAllByCategory(int categoryId)
+        {
+            return _products.Where(p => p.CtaegoryId == categoryId).ToList();
+        }
+
+        public void Update(Product product)
+        {
+            //Gönrderdiğim ürün id sine sahip olan listedeki ürünü bul
+            Product productToUpdate = _products.SingleOrDefault(p => p.ProductId == product.ProductId);
+            productToUpdate.ProductName = product.ProductName;
+            productToUpdate.CtaegoryId = product.CtaegoryId;
+            productToUpdate.UnitPrice = product.UnitPrice;
+            productToUpdate.UnitsInStock = product.UnitsInStock;
+
+        }
+    }
+}
