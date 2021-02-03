d1b0c380ab24b854cf235b6ea5cc0b0717bc0962
ayselmehmetzade <ayselmehmetzade@hotmail.com>
2021-02-03 23:47:41 +0300

    03.02.2021 kamp sonu

---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,22 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+
+    //generic canstraint= generic kısıt
+    //class: referans tip olabilir
+    //IEntity: IEntity olabilir ya da IEntityden implemente olanlar olabilir
+    public interface IEntityRepository<T> where T: class , IEntity
+    {
+        List<T> GetAll(Expression<Func<T,bool>> filter=null);
+        T Get(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+
+    }
+}
