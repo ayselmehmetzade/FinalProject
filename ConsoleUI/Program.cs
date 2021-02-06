0b56878fcad2eb579977101b05bf693e96188ced
ayselmehmetzade <ayselmehmetzade@hotmail.com>
2021-02-06 23:55:47 +0300

    6 şubat kamp sonu

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -9,6 +9,26 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
+            ProductTest();
+
+            //CategoryTest();
+
+
+
+
+        }
+
+        private static void CategoryTest()
+        {
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+            foreach (var category in categoryManager.GetAll())
+            {
+                Console.WriteLine(category.CategoryName);
+            }
+        }
+
+        private static void ProductTest()
+        {
             //ProductManager productManager = new ProductManager(new InMemoryProductDal());
             ProductManager productManager = new ProductManager(new EfProductDal());
 
@@ -23,10 +43,16 @@ namespace ConsoleUI
             //    console.writeline(product.productname);
             //}
 
-            foreach (var product in productManager.GetByUnitPrice(40,100))
+            //foreach (var product in productManager.GetByUnitPrice(40,100))
+            //{
+            //    Console.WriteLine(product.ProductName);
+            //}
+
+            foreach (var product in productManager.GetProductDetails())
             {
-                Console.WriteLine(product.ProductName);
+                Console.WriteLine(product.ProductName +" / "+ product.CategoryName);
             }
+
         }
     }
 }
--- Core/DataAccess/IEntityRepository.cs
+++ Core/DataAccess/IEntityRepository.cs
@@ -1,18 +1,18 @@
-﻿using Entities.Abstract;
+﻿using Core.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
 using System.Text;
 
-namespace DataAccess.Abstract
+namespace Core.DataAccess
 {
 
     //generic canstraint= generic kısıt
     //class: referans tip olabilir
     //IEntity: IEntity olabilir ya da IEntityden implemente olanlar olabilir
-    public interface IEntityRepository<T> where T: class , IEntity
+    public interface IEntityRepository<T> where T : class, IEntity, new()
     {
-        List<T> GetAll(Expression<Func<T,bool>> filter=null);
+        List<T> GetAll(Expression<Func<T, bool>> filter = null);
         T Get(Expression<Func<T, bool>> filter);
         void Add(T entity);
         void Update(T entity);
